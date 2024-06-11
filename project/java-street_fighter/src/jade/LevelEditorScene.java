d9c4365c3a4d10eb309e6916f099a3a19c6db40f
kiol <kioly.vikerg0412@gmail.com>
2024-06-11 21:07:18 +0800

    dirty flags

--- project/java-street_fighter/src/jade/LevelEditorScene.java
+++ project/java-street_fighter/src/jade/LevelEditorScene.java
@@ -2,12 +2,15 @@ package src.jade;
 
 import org.joml.Vector2f;
 
-import src.components.Sprite;
+// import src.components.Sprite;
 import src.components.SpriteRenderer;
 import src.components.Spritesheet;
 import src.util.AssetPool;
 
 public class LevelEditorScene extends Scene {
+    private final String filePath="assets/imgs/idel.png";
+    private GameObject obj1;
+    private Spritesheet sprites;
     public LevelEditorScene(){
     }
     @Override
@@ -15,23 +18,34 @@ public class LevelEditorScene extends Scene {
         loadResource();
         this.camera = new Camera(new Vector2f(-250, 0));
 
-        Spritesheet sprites = AssetPool.getSpritesheet("assets/imgs/ryu.jpg");
+        sprites = AssetPool.getSpritesheet(filePath);
 
-        GameObject obj1 = new GameObject("obj 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
+        obj1 = new GameObject("obj 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
         obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(obj1);
     }
     private void loadResource(){
         AssetPool.getShader("assets/shaders/default.glsl");
 
-        AssetPool.addSpritesheet("assets/imgs/ryu.jpg",
-                                new Spritesheet(AssetPool.getTexture("assets/imgs/ryu.jpg"),
-                                                64, 64, 5, 0));
+        AssetPool.addSpritesheet(filePath,
+                                new Spritesheet(AssetPool.getTexture(filePath),
+                                                128, 128, 5, 0));
     }
 
+    private int spriteIndex=0;
+    private float spriteFilpTime=0.2f;
+    private float spriteFilpTimeLeft=0.0f;
     @Override
     public void updata(float dt) {
         // System.out.println("FPS:"+(1.0f/dt));
+        spriteFilpTimeLeft-=dt;
+        if(spriteFilpTimeLeft<=0){
+            spriteFilpTimeLeft=spriteFilpTime;
+            spriteIndex++;
+            if(spriteIndex>4) spriteIndex=0;
+            obj1.getComponent(SpriteRenderer.class).setSprite(sprites.getSprite(spriteIndex));
+        }
+
         for(GameObject GO:this.gameObjects){
             GO.update(dt);
         }
